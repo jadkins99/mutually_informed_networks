198d853309030ab677998add5b46777ddae5cd79
David Sychrovský <dsych44@gmail.com>
2025-11-12 08:06:17 +0100

    Add MI visualization to training

--- mutually_informed_networks/src/experiments/train.py
+++ mutually_informed_networks/src/experiments/train.py
@@ -6,7 +6,9 @@ import optax
 
 from dataloader import dataloader
 from function_inequality_binary_classification import get_linear_dataset
+from layer_wise_mi import compute_layer_wise_mi
 from metrics import compute_accuracy
+from mi_plane import plot_mi_plane
 from mlp import MLP
 from cross_entropy import compute_bce_loss, compute_ce_loss
 from mod_n_classification import get_mod_n_dataset
@@ -39,16 +41,21 @@ def train_on_dataset(
     optim = optax.adam(learning_rate)
     opt_state = optim.init(model)
     losses = []
+    mi_with_input_history, mi_with_output_history = [], []
     for step, (x, y) in zip(range(steps), iter_data):
         loss, model, opt_state = make_step(model, x, y, optim, opt_state)
         loss = loss.item()
         print(f"step={step}, loss={loss}")
         losses.append(loss)
+        if (step + 1) % 10 == 0 and step > 50:
+            mi_with_input, mi_with_output = compute_layer_wise_mi(model, x, y)
+            mi_with_input_history.append(mi_with_input)
+            mi_with_output_history.append(mi_with_output)
 
     pred_ys = jax.vmap(model)(xs)
     final_accuracy = compute_accuracy(ys=ys, pred_ys=pred_ys)
     print(f"final_accuracy={final_accuracy}")
-    return model, xs, ys, pred_ys, losses
+    return model, xs, ys, pred_ys, losses, mi_with_input_history, mi_with_output_history
 
 
 if __name__ == "__main__":
@@ -56,10 +63,11 @@ if __name__ == "__main__":
     xs, ys = get_mod_n_dataset(dataset_size=10_000, key=data_key, n=4)
     model = MLP(in_size=xs[0].shape[-1], out_size=ys[0].shape[-1], width_size=32, depth=3, key=model_key)
 
-    model, xs, ys, pred_ys, losses = train_on_dataset(
+    model, xs, ys, pred_ys, losses, mi_input, mi_output = train_on_dataset(
         dataset=(xs, ys),
         model=model,
         batch_size=32,
         learning_rate=3e-3,
         steps=200,
     )
+    plot_mi_plane(mi_input, mi_output)
--- mutually_informed_networks/src/lib_visualization/mi_plane.py
+++ mutually_informed_networks/src/lib_visualization/mi_plane.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_mi_plane(mi_x_per_layer: list[list[float]], mi_y_per_layer: list[list[float]]):
@@ -10,21 +11,26 @@ def plot_mi_plane(mi_x_per_layer: list[list[float]], mi_y_per_layer: list[list[f
     :param mi_x_per_layer: For each layer, a list of mutual information values with the input along each epoch.
     :param mi_y_per_layer: For each layer, a list of mutual information values with the output along each epoch.
     """
-
-
     fig, ax = plt.subplots()
 
-    for layer_idx, (mi_x, mi_y) in enumerate(zip(mi_x_per_layer, mi_y_per_layer)):
+    mi_x_el = np.array(mi_x_per_layer)  # shape (num_steps, num_layers)
+    mi_y_el = np.array(mi_y_per_layer)  # shape (num_steps, num_layers)
+
+    for layer_idx in range(mi_x_el.shape[1]):
+        mi_x = mi_x_el[:, layer_idx].tolist()
+        mi_y = mi_y_el[:, layer_idx].tolist()
         _plot_layer_in_mi_plane(ax, mi_x, mi_y, layer_idx)
 
+    ax.grid()
     ax.set_xlabel('Mutual Information X')
     ax.set_ylabel('Mutual Information Y')
     ax.set_title('Mutual Information Plane')
+    ax.legend()
 
-    return ax
+    plt.savefig('mi_plane.png')  # TODO: save to a better place
 
 
 def _plot_layer_in_mi_plane(ax, mi_x: list[float], mi_y: list[float], layer_idx: int):
     """ Plots the mutual information trajectory of a single layer in the MI plane connected with a line. """
-    ax.scatter(mi_x, mi_y, s='-o', label=f'Layer {layer_idx}')
+    ax.plot(mi_x, mi_y, marker='o', linestyle='-', label=f'Layer {layer_idx}')
     return ax
