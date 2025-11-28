89dab97166af57690dda74a8130f87e75441f03b
David Sychrovský <dsych44@gmail.com>
2025-11-28 22:16:51 +0100

    fix: If requested dataset size is too large, enumerate all instead

--- mutually_informed_networks/src/lib_dataset_generator/mod_n_classification.py
+++ mutually_informed_networks/src/lib_dataset_generator/mod_n_classification.py
@@ -12,7 +12,12 @@ def get_mod_n_dataset(dataset_size: int, key: jrandom.PRNGKey, n: int = 2) -> tu
     :return: a pair of arrays of inputs and targets (0, 1, ..., n-1).
     """
     assert n < 2**8, f'Requested number of classes {n} is too large for 8-bit representation.'
-    x = jrandom.randint(key, (dataset_size, 1), minval=0, maxval=2**16)
+    if dataset_size > 2**8:
+        print(f"Warning: dataset_size {dataset_size} is larger than the number of unique 8-bit integers (256). "
+              f"Enumerating the whole space instead of sampling randomly.")
+        x = jnp.arange(0, 2**8).reshape(-1, 1)[jrandom.permutation(key, jnp.arange(2**8))]
+    else:
+        x = jrandom.randint(key, (dataset_size, 1), minval=0, maxval=2**8)
     y = x % n
     x_bits = jnp.unpackbits(x.astype('uint8')).reshape(len(x), 8)
     y_one_hot = jnp.eye(n)[y.flatten()]
