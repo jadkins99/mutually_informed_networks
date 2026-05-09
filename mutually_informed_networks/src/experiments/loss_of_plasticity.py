0b3677a2aaa97d7db6ecda61583f0181593cebd7
David Sychrovský <dsych44@gmail.com>
2026-05-09 22:39:15 +0200

    Cleanup

--- mutually_informed_networks/src/experiments/loss_of_plasticity.py
+++ mutually_informed_networks/src/experiments/loss_of_plasticity.py
@@ -5,57 +5,48 @@ import numpy as np
 from mi_plane import plot_mi_plane
 from mlp import MLP
 from mod_n_classification import get_mod_n_dataset
-from trajectory import train_on_dataset
+from trajectory import train_on_dataset, TrajectoryInfo
 
 
-def get_trajectory_losses(seed):
+def get_trajectory_losses(seed: int, num_classes: int, num_permutations: int) -> list[TrajectoryInfo]:
     data_key, model_key = jrandom.split(jrandom.PRNGKey(seed=0), 2)
-    xs, ys = get_mod_n_dataset(dataset_size=256, key=data_key, n=4)
+    xs, ys = get_mod_n_dataset(dataset_size=256, key=data_key, n=num_classes)
     model = MLP(in_size=xs[0].shape[-1], out_size=ys[0].shape[-1], layer_sizes=[4, 3, 2], key=model_key)
 
-    final_losses = []
-    final_accuracies = []
-    mi_inputs, mi_outputs = [], []
-    mi_input_diffss, mi_output_diffss = [], []
-    for step in range(20):
-        print(f"Training trajectory {step} with seed {step + seed}...")
-        data_key = jrandom.split(jrandom.PRNGKey(seed=step + seed), 1)[0]
-        xs, ys = get_mod_n_dataset(dataset_size=256, key=data_key, n=4, shuffle_bits=True)
+    trajectory_infos: list[TrajectoryInfo] = []
+    for traj_idx in range(num_permutations):
+        print(f"Training trajectory {traj_idx} with seed {traj_idx + seed}...")
+        data_key = jrandom.split(jrandom.PRNGKey(seed=traj_idx + seed), 1)[0]
+        xs, ys = get_mod_n_dataset(dataset_size=256, key=data_key, n=num_classes, shuffle_bits=True)
 
-        compute_mi = True#step < 4 or step % 10 == 0 or step == 49
-        model, losses, final_accuracy, mi_input, mi_output, mi_input_diffs, mi_output_diffs = train_on_dataset(
+        model, trajectory_info = train_on_dataset(
             dataset=(xs, ys),
             model=model,
             batch_size=32,
             learning_rate=1e-3,
             steps=1000,
-            compute_mi_every=100 if compute_mi else 0,
+            compute_mi_every=100,
         )
-        final_losses.append(losses[-1])
-        final_accuracies.append(final_accuracy)
-        if compute_mi:
-            mi_inputs.append(mi_input)
-            mi_outputs.append(mi_output)
-            mi_input_diffss.append(mi_input_diffs)
-            mi_output_diffss.append(mi_output_diffs)
+        trajectory_infos.append(trajectory_info)
 
-    return final_losses, final_accuracies, mi_inputs, mi_outputs, mi_input_diffss, mi_output_diffss
+    return trajectory_infos
 
 
 if __name__ == "__main__":
-    final_losses_per_seed = []
-    final_accuracies_per_seed = []
-    mi_inputs_per_seed, mi_outputs_per_seed = [], []
-    for seed in range(2):
-        final_losses, final_accuracies, mi_inputs, mi_outputs, mi_input_diffs, mi_output_diffs = get_trajectory_losses(seed)
-        final_losses_per_seed.append(final_losses)
-        final_accuracies_per_seed.append(final_accuracies)
-        mi_inputs_per_seed.append(mi_inputs)
-        mi_outputs_per_seed.append(mi_outputs)
+    num_seeds = 2
+    num_classes = 4
+    num_permutations = 10
 
-    mean = np.array(final_losses_per_seed).mean(0)
-    ste = np.array(final_losses_per_seed).std(0) / np.sqrt(len(final_losses_per_seed))
+    traj_infos_per_seed: list[list[TrajectoryInfo]] = []
+    for seed in range(2):
+        trajectory_infos = get_trajectory_losses(seed, num_classes, num_permutations)
+        traj_infos_per_seed.append(trajectory_infos)
+
+    final_loss_sp = np.array([[t.losses[-1] for t in t_infos] for t_infos in traj_infos_per_seed])  # shape (num_seeds, num_permutations)
+    mean = np.array(final_loss_sp).mean(0)
+    ste = np.array(final_loss_sp).std(0) / np.sqrt(len(traj_infos_per_seed))
     plt.grid(alpha=0.2)
+    plt.axhline(y=np.log(4), color='grey', linestyle='--', label='Random Guess Loss')
     plt.plot(mean)
     plt.fill_between(range(len(mean)), mean - ste, mean + ste, alpha=0.3)
     plt.ylim(ymin=0)
@@ -64,10 +55,12 @@ if __name__ == "__main__":
     plt.title('Final Losses Across Different Input Bit Permutations')
     plt.savefig('final_losses.png')
 
-    mean = np.array(final_accuracies_per_seed).mean(0)
-    ste = np.array(final_accuracies_per_seed).std(0) / np.sqrt(len(final_accuracies_per_seed))
+    final_accuracy_sp = np.array([[t.accuracies[-1] for t in t_infos] for t_infos in traj_infos_per_seed])  # shape (num_seeds, num_permutations)
+    mean = np.array(final_accuracy_sp).mean(0)
+    ste = np.array(final_accuracy_sp).std(0) / np.sqrt(len(traj_infos_per_seed))
     plt.clf()
     plt.grid(alpha=0.2)
+    plt.axhline(y=1 / num_classes, color='grey', linestyle='--', label='Random Guess Accuracy')
     plt.plot(mean)
     plt.fill_between(range(len(mean)), mean - ste, mean + ste, alpha=0.3)
     plt.ylim(0, 1.1)
@@ -76,34 +69,11 @@ if __name__ == "__main__":
     plt.title('Final Accuracies Across Different Input Bit Permutations')
     plt.savefig('final_accuracies.png')
 
-    mi_inputs = np.array(mi_inputs_per_seed).mean(0)
-    mi_outputs = np.array(mi_outputs_per_seed).mean(0)
-    for traj in range(len(mi_inputs)):
-        mi_input = mi_inputs[traj]
-        mi_output = mi_outputs[traj]
-
-        plot_mi_plane(mi_input, mi_output, idx=traj)
-
-    plt.clf()
-    plt.grid(alpha=0.2)
-    for layer, diffs in mi_input_diffs[0].items():
-        for neuron_idx, diff in enumerate(diffs):
-            label = layer+f' Neuron {neuron_idx}'
-            y = np.array([mi[layer][neuron_idx] for mi in mi_input_diffs])
-            plt.plot(y, label=label)
-    plt.xlabel('Iteration')
-    plt.ylabel('MI Drop')
-    plt.legend()
-    plt.savefig('mi_input_drops.png')
-
-    plt.clf()
-    plt.grid(alpha=0.2)
-    for layer, diffs in mi_output_diffs[0].items():
-        for neuron_idx, diff in enumerate(diffs):
-            label = layer+f' Neuron {neuron_idx}'
-            y = np.array([mi[layer][neuron_idx] for mi in mi_output_diffs])
-            plt.plot(y, label=label)
-    plt.xlabel('Iteration')
-    plt.ylabel('MI Drop')
-    plt.legend()
-    plt.savefig('mi_output_drops.png')
+    mi_inputs_spln = np.array([[t.mi_with_input for t in t_infos] for t_infos in traj_infos_per_seed])  # shape (num_seeds, num_permutations, num_layers, num_steps)
+    mi_outputs_spln = np.array([[t.mi_with_output for t in t_infos] for t_infos in traj_infos_per_seed])  # shape (num_seeds, num_permutations, num_layers, num_steps)
+    mi_inputs_pln = np.array(mi_inputs_spln).mean(0)
+    mi_outputs_pln = np.array(mi_outputs_spln).mean(0)
+    for traj_idx in range(len(mi_inputs_pln)):
+        mi_input = mi_inputs_pln[traj_idx]
+        mi_output = mi_outputs_pln[traj_idx]
+        plot_mi_plane(mi_input, mi_output, idx=traj_idx)
